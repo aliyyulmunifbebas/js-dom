ead0e1ce7c3ecff85f7429d101989a8149698c3a
aliyyulmunifbebas <rexixstore@gmail.com>
2025-12-03 13:08:26 +0700

    seleciton manipulation

---
+++ manipulation.js
@@ -0,0 +1,7 @@
+const judul = document.getElementById(`judul`)
+judul.innerHTML = `anjayy`
+judul.style.color = `green`
+judul.setAttribute(`class`,`judul`)
+
+const p2 = document.querySelector(`.p2`)
+p2.classList.add(`hhe`,`haha`)
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log(document)
+//DOM Selection
+//documen.getElementById()
+// const judul= document.getElementById(`judul`);
+// judul.style.color =`blue`
+// judul.style.fontSize = `100px`
+// judul.style.backgroundColor= `purple`
+// judul.innerHTML =`aliyyul`
+
+// //documen.getElemenByTagName()
+// const p = document.getElementsByTagName(`p`)
+// for(let i=0;i< p.length; i++){
+// p[i].style.backgroundColor=`lightblue`    
+// }
+
+// const h1 = document.getElementsByTagName(`h1`)[0]
+// //document.getElemenByClassName
+// const p1 = document.getElementsByClassName(`p1`)
+// p1[0].innerHTML=`ini diubah dalam js`
+
+
+// //document.querySelector()
+// const p4 = document.querySelector(`#b p`)
+// p4.style.color=`yellow`
+// const i2 = document.querySelector(`#b ul li:nth-child(2)`)
+
+// //document.querySelectorAll()
+// const pa = document.querySelectorAll(`p`)
